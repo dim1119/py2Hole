75133e65fc7f79291583ae5c2707e6ebe5e16071
dim1119 <dlazarak@gmail.com>
2019-04-25 15:36:36 +0300

    Code Cleanup

--- fetchFile.py
+++ fetchFile.py
@@ -1,9 +1,11 @@
 import base64
-#read the file, decode base64, then return it to the enable()/disable()
+
+
+# read the file, decode base64, then return it to the enable()/disable()
 def getApiKey():
     try:
-        raw=open('ApiKey.txt','r')
-        base=(raw.readline()).split(':')[1]
+        raw = open('ApiKey.txt', 'r')
+        base = (raw.readline()).split(':')[1]
         raw.close()
         return base64.b64decode(base)
 
@@ -11,16 +13,18 @@ def getApiKey():
         print("File not accessible")
         return 0
 
-#bugs-----\/-----\/----
+
+# bugs-----\/-----\/----
 def createFile():
     try:
-        raw=open('ApiKey.txt','r')
+        raw = open('ApiKey.txt', 'r')
         raw.close()
     except:
-        raw=open('ApiKey.txt','w')
-        usrInput=input("Enter your api key and press enter: ")
-        raw.write("apikey:"+base64.b64encode(usrInput).encode('ascii'))
+        raw = open('ApiKey.txt', 'w')
+        usrInput = input("Enter your api key and press enter: ")
+        raw.write("apikey:" + base64.b64encode(usrInput).encode('ascii'))
         raw.close()
+
 
 if __name__ == "__main__":
     createFile()
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import argparse
 import Parser
+
+
 def main():
-    #using argparse for the arguments
+    # using argparse for the arguments
     parser = argparse.ArgumentParser(description="Pihole API interface")
     parser.add_argument('-v', '--version', action='version',version='%(prog)s 1.0')
-    parser.add_argument('command', choices=Parser.FUNCTION_MAP.keys()) #getting the appropriate functions from FUNCTION_MAP
+    parser.add_argument('command',
+                        choices=Parser.FUNCTION_MAP.keys())  # getting the appropriate functions from FUNCTION_MAP
     args=parser.parse_args()
-    func = Parser.FUNCTION_MAP[args.command] #calling the appropriate function
+    func = Parser.FUNCTION_MAP[args.command]  # calling the appropriate function
     print(func())
-#   print("Help: \n-clients\tUnique clients \n-status\tStatus of the PiHole DNS Server \n-privacy_level\tPrivacy mode\nads_percentage\tThe percentage of ads blocked today")    
-    
 
 
 if __name__ == "__main__":
